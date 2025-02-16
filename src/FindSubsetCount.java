55a1c25688fc0e9497ebcf68f14cfa3d3809f50a
Zain-Abbas <zain786@outlook.com>
2025-02-15 16:54:03 -0800

    Still trying to find solution that runs in O(N^2 * Log(N) time

--- src/FindSubsetCount.java
+++ src/FindSubsetCount.java
@@ -125,10 +125,11 @@ public class FindSubsetCount {
 
         ///  create the PrefixSum2D object
         PrefixSum2D prefixSum = new PrefixSum2D(matrix);
-
         System.out.println("PrefixSum: " + prefixSum);
+        //prefixSum.printPreSum();
+        // System.out.println("PrefixSum: " + prefixSum);
         /// apply recursive algorithm that calculates the preSum for rectangle
-         /// sumRegion -> determines #of points within the rectangle
+        /// sumRegion -> determines #of points within the rectangle
 
 
         /// TODO: next steps
@@ -141,29 +142,37 @@ public class FindSubsetCount {
         ///  process:
         ///  1) create matrix Matrix that stores all of the points in 2D array
         ///  2) will need to pick arbitrary 2 points (vertex in entire graph)
-          ///  ex) (i,j) = 0
+        ///  ex) (i,j) = 0
         ///  3) do rectangle process (determining going left or right)
         ///  rangeQuery to calculate number of points to determine direction
 
-        
+
         //
-         // left = x-coordinate
-         // right =
+        // left = starting column of submatrix
+        // right = represents the ending column of submatrix
+        // ex)
+        // rowSum[] -> for each pair of left and right calculate sum of points in
+        // a specific row
+        // if rowSum[row] = 0 // no point contained in submatrix
+        // if rowSum[row] = 1 // point contained in submatrix
         for (int left = 0; left < numX; left++) {
-            for (int right = left + 1; right < numX; right++) {
+            for (int right = left+1; right < numX; right++) {
                 // storing the sum of points in each row between the columns left
-              //  and right
+                //  and right
                 // ex)
                 int[] rowSum = new int[numY];
+               // System.out.println("left = " + left + ", right = " + right);
                 for (int row = 0; row < numY; row++) {
-                    rowSum[row] = prefixSum.sumRegion(left, row, right, row);
-                    System.out.print(rowSum[row] + " ");
+                    System.out.println("prefixSum.sumRegion(" + left + ", " + row + ", " + right + ", " + row + ")");
+                    int sumInRow = prefixSum.sumRegion(left, row, right, row);
+                    rowSum[row] = sumInRow;
+                    System.out.print("rowSum[" + row + "] = " + rowSum[row] + " ");
                 }
-               // count += countValidRectangles(rowSum);
+                System.out.println();
+                // count += countValidRectangles(rowSum);
             }
         }
 
-         
 
         long count = 0;
         /* your code here to calculate the count*/
@@ -190,6 +199,7 @@ class PrefixSum2D {
                 // calculate the sum of elements for each matrix [0,0,i,j]
                 preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1]
                         + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
+
             }
         }
         for (int i = 1; i <= m; i++) {
@@ -209,7 +219,51 @@ class PrefixSum2D {
 
     // calculate the sum of elements in the submatrix [x1, y1, x2, y2]
     public int sumRegion(int x1, int y1, int x2, int y2) {
-        return preSum[x2 + 1][y2 + 1] - preSum[x2 + 1][y1] + preSum[x1][y1] - preSum[x1][y2+1];
+       // return preSum[x2 + 1][y2 + 1] - preSum[x2 + 1][y1] + preSum[x1][y1] - preSum[x1][y2+1];
+       // return preSum[x2+1][y2+1] - preSum[x1][y2+1] - preSum[x2+1][y1] + preSum[x1][y1];
+        // debugging:
+      //  System.out.println("x1: " + x1 + ", y1: " + y1 + ", x2: " + x2 + ", y2: " + y2);
+       /*
+        int a = preSum[x2 + 1][y2 + 1];
+        int b = preSum[x1 + 1][y2 + 1];
+        int c = preSum[x2 + 1][y1];
+        int d = preSum[x1][y2];
+        System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+        return a - b - c + d;
+
+
+        int a = preSum[x2+1][y2+1];
+        System.out.println("a = " + a);
+        int b = preSum[x2+1][y1];
+        System.out.println("b = " + b);
+        int c = preSum[x1][y1];
+        System.out.println("c = " + c);
+        int d = preSum[x1][y2+1];
+        System.out.println("d = " + d);
+        //System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+        return a - b + c - d;
+        */
+        // Adjust the indices to account for 1-indexed preSum matrix
+        int a = preSum[x2 + 1][y2 + 1];   // bottom-right corner
+        int b = preSum[x1][y2 + 1];       // top-right corner (row above)
+        int c = preSum[x2 + 1][y1];       // bottom-left corner (column before)
+        int d = preSum[x1][y1];           // top-left corner (to add back, as it's subtracted twice)
+        int sum = a - b - c + d;
+        System.out.println("Sum: " + sum);
+        // Debugging output for checking each term
+        System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+
+        // Calculate the sum for the submatrix by applying the inclusion-exclusion principle
+        return sum;
+    }
+
+    public void printPreSum() {
+        for (int i = 1; i <= preSum.length; i++) {
+            for (int j = 1; j <= preSum[0].length; j++) {
+                System.out.print(preSum[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 
